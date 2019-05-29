42c4aba04199c4d4e6b70ea341caf92faa9b2927
RickN123 <nicolai.richard@gmail.com>
2019-05-28 21:01:09 -0400

    avenger update score

--- react-clickygame/src/App.js
+++ react-clickygame/src/App.js
@@ -8,7 +8,7 @@ import friends from "./friends.json";
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = array[j], array[i]];
+    [array[i], array[j]] = [array[j], array[i]];
 
   }
   return array;
@@ -17,61 +17,65 @@ function shuffle(array) {
 class App extends Component {
   // Setting this.state.friends to the friends json array
   constructor(props) {
+    super(props);
     this.state = {
-      friends
-    score: 0,
+      friends,
+      score: 0,
       highScore: 0,
       clickedAvengers: []
     };
 
-    clickedFriend = id => {
+  }
+  
+  clickedFriend = id => {
 
-      if (this.state.clickedAvengers.includes(id)) {
-        alert("This Avenger has been clicked!  You must restart!");
+    if (this.state.clickedAvengers.includes(id)) {
+      alert("This Avenger has been clicked!  You must restart!");
+      this.setState({ score: 0 });
+      this.setState({ clickedAvengers: [] });
+    } else {
+
+      this.setState({ clickedAvengers: [...this.state.clickedAvengers, id] });
+
+      const newScore = this.state.score + 1;
+      this.setState({
+        score: newScore
+      });
+
+      if (newScore > this.state.highScore) {
+        this.setState({ highScore: newScore });
+      }
+
+      if (newScore === 11) {
+        alert("Congratulations, You Won!!");
         this.setState({ score: 0 });
         this.setState({ clickedAvengers: [] });
-      } else {
-
-        this.setState({ clickedAvengers: [...this.state.clickedAvengers, id] });
-
-        const newScore = this.state.score + 1;
-        this.setState({
-          score: newScore
-        });
-
-        if (newScore > this.state.highScore) {
-          this.setState({ highScore: newScore });
-        }
-
-        if (newScore === 11) {
-          alert("Congratulations, You Won!!");
-          this.setState({ score: 0 });
-          this.setState({ clickedAvengers: [] });
-        }
       }
     }
-
-
-
-
-    // Map over this.state.friends and render a FriendCard component for each friend object
-    render() {
-      return (
-        <Wrapper>
-          <Title>Avengers Friends!</Title>
-          {this.state.friends.map(friend => (
-            <FriendCard
-
-              id={friend.id}
-              key={friend.id}
-              name={friend.name}
-              image={friend.image}
-
-            />
-          ))}
-        </Wrapper>
-      );
-    }
   }
+  
 
-  export default App;
+
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Avengers Friends!</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
